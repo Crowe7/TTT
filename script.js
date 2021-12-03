dcf67b1a3d1795e03da1d46f483ca6c37b829874
Crowe7 <jacobocrowe@gmail.com>
2021-12-02 20:18:04 -0600

    added ability to assign a player based on which button clicked

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gameBoard = (() => {
-    let _gameBoard = ["X", "O", "X", "O", "X", "O", "X", "O","X"];
+    let _gameBoard = ["", "", "", "", "", "", "", "",""];
 
     function  _makeBoard() {
         for(i = 1; i <= 9; i++) {
@@ -20,11 +20,46 @@ const gameBoard = (() => {
     
     return {
         displayCurrentBoard,
+
     }
 
-})()
+})();
 
-gameBoard.displayCurrentBoard();
+// gameBoard.displayCurrentBoard();
+
+const Player = (name, sign) => {
+
+    return {name, sign};
+};
+
+const displayController = (() => {
+    let p1 = document.querySelector('#player1');
+    let p2 = document.querySelector('#player2');
+    let reset = document.querySelector('#reset');
+
+    function setPlayer() {
+        this.innerText = 'YOU';
+        if(this === p1) {
+            p2.innerText = 'CPU';
+            const player1 = Player('YOU', "X");
+            const player2 = Player('CPU', "O");
+        }
+        else {
+            p1.innerText = 'CPU';
+            const player1 = Player('CPU', "X");
+            const player2 = Player('YOU', "O");
+        }
+    }
+
+    p1.addEventListener('click', setPlayer);
+    p2.addEventListener('click', setPlayer);
+
+})();
+
+// maybe add a function to gameboard that allows the ability to pass a param based on div clicked
+// and what player to change the array and then run displaycurrntboard
 
 //display controller will be the "victory screen" and the player buttons and reset button 
+//will also but a border around the current players button 
 
+//maybe add 

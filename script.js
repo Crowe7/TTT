bc1eca62b20a20e782e0c16ff5a24852c95a0316
Crowe7 <jacobocrowe@gmail.com>
2021-12-03 00:45:14 -0600

    set current player to be highlighted

--- script.js
+++ script.js
@@ -27,9 +27,9 @@ const gameBoard = (() => {
 
 // gameBoard.displayCurrentBoard();
 
-const Player = (name, sign) => {
+const Player = (sign) => {
 
-    return {name, sign};
+    return {sign};
 };
 
 const displayController = (() => {
@@ -37,24 +37,79 @@ const displayController = (() => {
     let p2 = document.querySelector('#player2');
     let reset = document.querySelector('#reset');
 
-    function setPlayer() {
-        this.innerText = 'YOU';
-        if(this === p1) {
+    function setPlayer(player) {
+        if(player === p1) {
+            p1.innerText = 'YOU';
             p2.innerText = 'CPU';
-            const player1 = Player('YOU', "X");
-            const player2 = Player('CPU', "O");
         }
         else {
+            p2.innerText = 'YOU';
             p1.innerText = 'CPU';
-            const player1 = Player('CPU', "X");
-            const player2 = Player('YOU', "O");
         }
     }
 
-    p1.addEventListener('click', setPlayer);
-    p2.addEventListener('click', setPlayer);
+    p1.addEventListener('click', () => {
+        setPlayer(p1);
+        currentPlayer();
+    });
+    p2.addEventListener('click', () => {
+        setPlayer();
+        currentPlayer();
+    });
+
+    function currentPlayer() {
+        if(playRound.playTurn() === 'X') {
+            p1.classList.add('on');
+            if(p2.classList.contains('on')) {
+                p2.classList.remove('on');
+            }
+        }
+        else {
+            p2.classList.add('on')
+            p1.classList.remove('on');
+        }
+    }
+
+
+
+    return {
+        p1,
+        p2,
+    }
 
 })();
+
+const playRound = (() => {
+    const player1 = Player("X");
+    const player2 = Player("O");
+    let turnCounter = 2;
+
+    function playTurn() {
+        if(turnCounter % 2 === 0) {
+            turn = player1.sign;
+            console.log(turn)
+        }
+        else {
+            turn = player2.sign;
+            console.log(turn);
+        }
+        turnCounter++
+        return turn;
+    }
+
+    return {
+        playTurn,
+    }
+})();
+
+function currentPlayer() {
+    if(playRound.playTurn() === 'X') {
+        console.log('e');
+    }
+    else {
+        console.log(playRound.playTurn);
+    }
+}
 
 // maybe add a function to gameboard that allows the ability to pass a param based on div clicked
 // and what player to change the array and then run displaycurrntboard
@@ -62,4 +117,9 @@ const displayController = (() => {
 //display controller will be the "victory screen" and the player buttons and reset button 
 //will also but a border around the current players button 
 
-//maybe add 
+//TODO ADD RESET FUNTIONALITY
+
+
+
+
+//
